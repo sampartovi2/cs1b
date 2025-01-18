3646d75f6e80d558d733f2aee22777e01f3dc2df
sampartovi2 <spartovi1@saddleback.edu>
2025-01-18 18:10:16 +0000

    first edit

---
+++ S01/program02.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main(){
+    string file = "aaaddd";
+    cout << sizeof(file);
+    //for(int i = 0; i < file.length; i++);
+    
+}
